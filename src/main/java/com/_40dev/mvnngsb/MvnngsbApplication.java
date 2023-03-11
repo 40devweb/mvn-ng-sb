000a9ee42707a7c9b508d9a9966ed08e71d562bb
shnishi <shigeonishi@hotmail.com>
2023-03-11 10:08:11 -0500

    Updated source for Spring Boot 3

---
+++ src/main/java/com/_40dev/mvnngsb/MvnngsbApplication.java
@@ -0,0 +1,13 @@
+package com._40dev.mvnngsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnngsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnngsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_40dev/mvnngsb/MvnngsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com._40dev.mvnngsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnngsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
